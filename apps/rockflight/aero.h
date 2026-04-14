7c20380958c882f33663e228f7381fbb9b2143eb
Alberto <albertoxscomparin@gmail.com>
2026-04-14 17:50:14 +0200

    Remove older rockflight files, already in lib.

--- apps/rockflight/aero.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef __aero_h__
-#define __aero_h__
-
-#include "state.h"
-
-int aero(double *y, state_t *s);
-
-#endif /* __aero_h__ */
-
--- apps/rockflight/engine.h
+++
@@ -1,8 +0,0 @@
-﻿
-#ifndef __engine_h__
-#define __engine_h__
-
-int engine(double *y, state_t *s);
-
-#endif /* __engine_h__ */
-
